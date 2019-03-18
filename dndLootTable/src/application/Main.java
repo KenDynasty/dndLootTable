b6cae430271745613247dbaab0e4f9151002a309
halen <halen@WIT45005737.wit.private>
2019-03-17 21:17:17 -0400

    Setting up files and testing how to make scenes transition

--- dndLootTable/src/application/Main.java
+++ dndLootTable/src/application/Main.java
@@ -1,20 +1,38 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			VBox Types = new VBox();
+		
+			Button next = new Button("next");
+			Scene scene = new Scene(next);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			EnemyEncounter enemies = new EnemyEncounter();
+			treasureChest treasure = new treasureChest();
+			Object Select = treasure;
+			
+			
+			
+			next.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					enemies.updateParameters(primaryStage);
+				}
+			});
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ dndLootTable/src/application/treasureChest.java
@@ -0,0 +1,21 @@
+package application;
+
+public class treasureChest extends LootEvent implements RollItem {
+
+	@Override
+	public String roll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String reroll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public treasureChest() {
+		
+	}
+
+}

e6fb1c6aebfa772641faeaf369f5eff9a3d98e1b
halen <halen@WIT45005737.wit.private>
2019-03-26 17:53:17 -0400

    Created final scene

--- dndLootTable/src/application/LootEvent.java
+++ dndLootTable/src/application/LootEvent.java
@@ -11,7 +11,6 @@ public abstract class LootEvent {
 	public static boolean magicItems;
 	public static int numItems;
 	public static String[]loot;
-	public int sceneCount = 0;
 	public  Scene[] scenes;
 	
 	public LootEvent() {
@@ -34,23 +33,7 @@ public abstract class LootEvent {
 	
 	public void updateParameters(Stage primaryStage) {
 		
-		Button yee = new Button("it WORKED");
-		VBox test = new VBox();
-		test.getChildren().add(yee);
-		Scene test1 = new Scene(test, 400, 400);
-		primaryStage.setScene(test1);
-		/*sceneCount++;
-		if(sceneCount == 0) {
-			difficulty = getdifficulty();
-		}
-		else if(sceneCount == 1) {
-			magicItems = getmagicItems();
-			mundaneItems = getMundaneItems();
-		}
-		else if(sceneCount == 2) {
-			
-		}
-		*/
+		
 	}
 
 }
